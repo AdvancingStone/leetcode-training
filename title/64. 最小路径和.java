11620f7d1c313fc3d0774fb54ef596c012dcc1fa
AdvancingStone <31798297+AdvancingStone@users.noreply.github.com>
2020-09-28 16:54:06 +0800

    Create 64. 最小路径和.java

---
+++ title/64. 最小路径和.java
@@ -0,0 +1,21 @@
+/**
+ * 64. 最小路径和
+ * https://leetcode-cn.com/problems/minimum-path-sum/
+ */
+
+class Solution {
+    public int minPathSum(int[][] grid) {
+        for (int i=1; i<grid.length; i++){
+            grid[i][0] = grid[i][0] + grid[i-1][0];
+        }
+        for (int i=1; i<grid[0].length; i++){
+            grid[0][i] = grid[0][i] + grid[0][i-1];
+        }
+        for (int i=1; i<grid.length; i++){
+            for (int j=1; j<grid[0].length; j++){
+                grid[i][j] = grid[i][j] + Math.min(grid[i-1][j], grid[i][j-1]);
+            }
+        }
+        return grid[grid.length-1][grid[0].length-1];
+    }
+}
