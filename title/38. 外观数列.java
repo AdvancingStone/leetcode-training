694078b4d5bb07b7d30399df2f4f2ff0596aed09
AdvancingStone <31798297+AdvancingStone@users.noreply.github.com>
2020-10-15 14:27:20 +0800

    Update 38. 外观数列.java

--- title/38. 外观数列.java
+++ title/38. 外观数列.java
@@ -7,19 +7,19 @@ class Solution {
     public String countAndSay(int n) {
         String result = "1";
         for (int i = 0; i < n-1; i++) {
-            result = readString(result, 0);
+            result = readString(result);
         }
         return result;
     }
-    public String readString(String res, int index) {
+    public String readString(String res) {
         int n = res.length();
         int count = 1;
-        StringBuffer br = new StringBuffer();
-        for (int i = index; i < n; i++) {
+        StringBuilder br = new StringBuilder();
+        for (int i = 0; i < n; i++) {
             if (i + 1 < n && res.charAt(i + 1) == res.charAt(i)) {
                 count++;
             } else {
-                br.append("" + count + res.charAt(i));
+                br.append(count).append(res.charAt(i));
                 count = 1;
             }
         }
